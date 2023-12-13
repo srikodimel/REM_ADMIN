af6a559a4d9154ac3842c2dd4396a78693b9bd72
srikodimel <152472618+srikodimel@users.noreply.github.com>
2023-12-13 16:16:06 +0530

    Add files via upload

---
+++ let,const.js
@@ -0,0 +1,14 @@
+
+//let hoisting
+a=10;
+let a;
+console.log(a);
+
+//Const hoisting
+b=20;
+let b;
+console.log(b);
+
+
+
+
---
+++ varhoisting.js
@@ -0,0 +1,18 @@
+//var hoisting
+
+c=10;
+var c;
+console.log("hoisting of ",c);
+function prepareObject(){
+    let Obj ={
+       name:'Srinu',
+       propertyType:'type',
+       propertyDesc:'Description',
+     
+
+    }
+    
+    alert(Obj);
+}
+//prepareObject()
+
